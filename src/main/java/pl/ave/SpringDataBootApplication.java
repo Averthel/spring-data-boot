9caeaceb10d3731638619a5cc983db1239b58237
Michał <42038384+Averthel@users.noreply.github.com>
2019-04-25 17:15:08 +0200

    detailed questions

--- src/main/java/pl/ave/SpringDataBootApplication.java
+++ src/main/java/pl/ave/SpringDataBootApplication.java
@@ -17,17 +17,41 @@ public class SpringDataBootApplication {
 
 
         List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Ford", "Focus", 65000.0));
-        cars.add(new Car("Ford", "Fiesta", 45000.0));
-        cars.add(new Car("Ford", "Mondeo", 85000.0));
+        cars.add(new Car( "Focus","Ford", 65000.0));
+        cars.add(new Car( "Fiesta","Ford", 45000.0));
+        cars.add(new Car( "Mondeo", "Ford",85000.0));
+        cars.add(new Car( "Yaris","Toyota", 11000.0));
+        cars.add(new Car( "Civic","Honda", 8000.0));
+        cars.add(new Car("X5", "BMW", 80000.0));
+        cars.add(new Car( "Q5", "Audi",110000.0));
+        cars.add(new Car( "Omega","Opel", 5000.0));
 
         CarRepository carRepo = ctx.getBean(CarRepository.class);
-        cars.forEach(carRepo::save);
+        cars.forEach(carRepo::save); //Zapisanie samochodów do bazy
 
-        Car firstCar = carRepo.findById(1L).get();
-        carRepo.delete(firstCar);
+        System.out.println("First By Brand: "); //First
+        Car firstByBrand = carRepo.findFirstByBrand("Ford");
+        System.out.println(firstByBrand);
+
+        System.out.println("All Fords"); //All
+        List<Car> fords = carRepo.findByBrand("Ford");
+        fords.forEach(System.out::println);
+
+        //And / Or
+        System.out.println("Ford for 45000");
+        carRepo.findAllByBrandAndPrice("Ford", 45000).forEach(System.out::println);
+        System.out.println("lub: ");
+        carRepo.findAllByBrandOrBrand("BMW", "Audi").forEach(System.out::println);
+
+        //lessThan, between
+        System.out.println("Cars cheaper than 30000");
+        carRepo.findAllByPriceLessThan(30000).forEach(System.out::println);
+        System.out.println("Cars with price between 40-80k");
+        carRepo.findAllByPriceBetween(40000, 80000).forEach(System.out::println);
 
         carRepo.findAll().forEach(System.out::println);
+
+        ctx.close();
     }
 
 }
--- src/main/java/pl/ave/repository/CarRepository.java
+++ src/main/java/pl/ave/repository/CarRepository.java
@@ -4,7 +4,67 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.ave.model.Car;
 
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+    /*
+    count() - zliczailość rekordów w danym repozytorium (ilość rekordów w bazie danych),
+    deleteById(id) - usuwa rekord o wskazanym id,
+    deleteAll(Iterable) - usuwa z bazy danych encje zapisane w kolekcji przekazanej jako argument metody. Kolekcja powinna implementować interfejs Iterable,
+    delete(entity) - usuwa z repozytorium encję przekazaną jako argument,
+    deleteAll() - usuwa z repozytorium wszystkie rekordy,
+    existsById(id) - sprawdza istnieje rekordu o wskazanym id,
+    findAll() - zwraca kolekcję wszystkich obiektów w repozytorium,
+    findAllById(ids) - zwraca kolekcję obiektów, których id znalazło się w kolekcji przekazanej jako argument,
+    findById(id) - zwraca z repozytorium Optionala z obiektem o wskazanym id,
+    save(entity) - zapisuje w repozytorium wszystkie rekordy z kolekcji przekazanej jako argument,
+    saveAll(iterable) - zapisuje w repozytorium pojedynczy obiekt.
+    */
+
+    List<Car> findByBrand(String brand);
+
+    //pierwszy samochód danej marki
+    Car findFirstByBrand(String brand);
+
+    //pierwszy samochód ze wskazaną ceną
+    Car findFirsByPrice(double price);
+    // lub alternatywnie:
+    //Car findTopByPrice(double price);
+
+    //pierwsze trzy samochody z bazy
+    List<Car> findFirst3ByBrand(String brand);
+
+    //samochody danej marki o wskazanej cenie
+    List<Car> findAllByBrandAndPrice(String brand, double price);
+
+    //samochody jednej z dwóch marek
+    List<Car> findAllByBrandOrBrand(String brand1, String brand2);
+
+    //samochody z marką określoną przez wzorzec, np. "A%" - rozpoczynające się od A
+    List<Car> findAllByBrandLike(String pattern);
+
+    //samochody o nazwach kończących się na wskazany ciąg znaków
+    List<Car> findAllByNameEndingWith(String pattern);
+
+    //samochody o cenach niższych od
+    List<Car> findAllByPriceLessThan(double price);
+
+    //samochody o cenie pomiędzy low i high
+    List<Car> findAllByPriceBetween(double low, double high);
+
+    //wszystkie samochody danego producenta z ceną rosnąco
+    List<Car> findAllByBrandOrderByPriceAsc(String brand);
+
+    //wszystkie samochody z ustawioną marką
+    List<Car> findAllByBrandNotNull();
+
+    //wszystkie samochody z nazwą należącą do kolekcji
+    List<Car> findAllByNameIn(Collection<String> names);
+
 }
+
+
+
